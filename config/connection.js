e3463ad03dccf94b0d5c5fdc1b70a273f5563a91
Shi Sha <emailtotheowner@gmail.com>
2020-05-14 07:24:13 -0400

    Heroku deployment set up info and code writing completed.

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,21 @@
 const mysql = require("mysql");
 
 // creating connection to mysql database server
-const connection = mysql.createConnection({
+// adding Heroku connection information after subscribing with CC to Heroku host
+// ####### ADDED BLOCK BELOW #########
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+// ####### ADDED BLOCK ABOVE #########
+connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Stanopal001$",
   database: "burgers_db"
 });
-
+}; // This closing curly-bracket is also a part of the added block. Closing else statement!
 connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
